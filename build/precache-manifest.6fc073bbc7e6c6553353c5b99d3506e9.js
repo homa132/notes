bea66deb04596d5076487c5d54230127fb701aed
Andrey <homin2001@gmail.com>
2019-03-28 16:14:23 +0200

    release v 1.0

---
+++ build/precache-manifest.6fc073bbc7e6c6553353c5b99d3506e9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "37939d41cfd05189e078",
+    "url": "/static/js/main.9f8e8bf6.chunk.js"
+  },
+  {
+    "revision": "34af514d763d8d56275d",
+    "url": "/static/js/2.e81b9cbb.chunk.js"
+  },
+  {
+    "revision": "37939d41cfd05189e078",
+    "url": "/static/css/main.0af3f707.chunk.css"
+  },
+  {
+    "revision": "9d225456db1ad2b791c162c4467a52d7",
+    "url": "/index.html"
+  }
+];
